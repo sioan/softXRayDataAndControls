4566e47857cc5097b2cb47f674cadc2ee108c0c6
Sioan <zohar.sioan@gmail.com>
2017-06-30 17:24:06 -0700

    using screen to attach to MPI busb

---
+++ psanaExperimentation/screenSetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#runs on psdev7a
+#screen -X -S psanaScreen quit
+
+
+screen -d -m -S psanaScreen
+screen -S psanaScreen -X stuff 'cd /reg/neh/home/sioan/Desktop/softXRayDataAndControls/psanaExperimentation \n'
+screen -S psanaScreen -X stuff 'python -i boringWhile.py \n'
