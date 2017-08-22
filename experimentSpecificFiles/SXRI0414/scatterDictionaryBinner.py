9b267d3f9ccb7dd3203c253dd92a492ee35dfe41
Sioan <zohar.sioan@gmail.com>
2017-08-22 10:47:27 -0700

    scatter binner

--- experimentSpecificFiles/SXRI0414/scatterDictionaryBinner.py
+++ experimentSpecificFiles/SXRI0414/scatterDictionaryBinner.py
@@ -26,8 +26,8 @@ import h5py
 #this is being abstracted from previous attempts to analyze SXRI0414.  how to abstract the approaches taken so far. they are
 #1) the brute force standard deviation and mean.  (original provided by h5 file by pi.  gives the slow oscillation without much effort.)
 #2) the elegant filtering using glue to get rid of zeros.
-#3) and projection onto calibration curve gave nice oscillations.
-#4) parsing the different scans.  didn't give oscillations. nice scans. but fiducials odd vs even may not correspond to 
+#3) and projection onto calibration curve gave nice slow oscillations.
+#4) parsing the different scans.  didn't give oscillations. nice scans. fiducials odd vs even do correspond to event code 141
 
 #4) binning acqiris/gmd for different delay stages (this is important. close to brute force)/. result is 2d. gona fit this data. want to understand maths of 
 #fitting binned data.  This wasn't brute, but projection onto acqiris vs gmd calibration using calibration i0 as errorless x. not correct. but total least squares has fudge factor for weighting different detectors.
@@ -40,7 +40,7 @@ import h5py
 #how to 3d and 2d data sets, abstracting is almost intuitive, but still needs some thought.  parsing each delay stage scan. the result is.... that's binned... into big time buckets. roughly equal length. so it's a 3d data set? yes. acqiris/gmd, delay stage, and big time buckets that correspond to human experienced time needed to finish one of the scans.  that becomes a 3d cube. if broken up into acqiris, gmd, delay stage, and scan #, that becomes a 4d cube.
   
 
-#histogramdd. that should bin is as desired.  but how to get it into a format other programs like glue or imagej can use?  how to 
+#histogramdd. that should bin is as desired.  but how to get it into a format other programs like glue or imagej can use?  how to to make it so imagej's dynamic profiler can work on it without discounting weighting from 
 
 #which way to plot? Which way to sum? Which slice or projection?  along which axis?
 
@@ -51,6 +51,12 @@ import h5py
 #notes from josh for sxri. doesn't belong here, needs to find a home.
 #x-value (time in ps) = x-value (position of stage in mm) * 2 / c0
 #where c0 should be 0.299792458 (speed of light in mm/ps)
+
+#==================
+#final product
+
+
+
 
 #==================
 
@@ -80,7 +86,7 @@ if __name__ == '__main__':
 	myParser = argparse.ArgumentParser(description='Abstracts data analysis into user functions')
 		
 	myParser.add_argument('-f','--file name', help='file name')
-	myParser.add_argument('-d','--run',type=int,help='dictionary keys identifying data to bin')
+	myParser.add_argument('-d','--run',type=int,help='dictionary keys identifying data to bin',default=None)
 	myParser.add_argument('-c','--configuration file if the flags here are not helpful ',help='the config file to read from')
 	myParser.add_argument('-hd5','--hd5File',help='extension of the small data file to write to. typically a,b or c',default="")
 	myParser.add_argument('-t','--testSample',action='store_true',help='only take a small set of data for testing')
