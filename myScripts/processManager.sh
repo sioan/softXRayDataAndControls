d0e534b34a34d85f40033b9203922bfe5f565b32
Sioan <zohar.sioan@gmail.com>
2017-08-24 13:14:40 -0700

    merging

--- myScripts/processManager.sh
+++ myScripts/processManager.sh
@@ -1,3 +1,6 @@
 #this script will help get rid of my processes I forget to close on various systems.
 
-
+#generate the host names from netconfig
+#make a dat file with their names
+#ssh to them and check for screens
+#kill screen or get information about it.
