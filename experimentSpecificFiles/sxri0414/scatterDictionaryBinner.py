4df263994f809dd23a29a12eb31cd3b596bf476b
Sioan <zohar.sioan@gmail.com>
2017-08-29 10:12:36 -0700

    clean up

--- experimentSpecificFiles/sxri0414/scatterDictionaryBinner.py
+++ experimentSpecificFiles/sxri0414/scatterDictionaryBinner.py
@@ -358,5 +358,5 @@ myBinAverage = averageShiftedHistogram(toBeBinned[:,1],0,21,0.1, toBeBinned[:,0]
 myBinCount = averageShiftedHistogram(toBeBinned[:,1][::-1],0,21,0.1, ones(len(toBeBinned[:,0])),50)
 plot(myBinAverage[1][::-1],myBinAverage[0]/myBinCount[0])
 show()
-#execfile("gaussianKdeAdapted.py")
+execfile("gaussianKdeAdapted.py")
 
