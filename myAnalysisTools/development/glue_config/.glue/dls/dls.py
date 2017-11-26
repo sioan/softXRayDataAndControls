2e0dfdd39cc82d89477c85907ccb94c5435ffc10
Sioan <zohar.sioan@gmail.com>
2017-11-26 10:34:37 -0800

    working interactive demonstration

--- myAnalysisTools/development/glue_config/.glue/dls/dls.py
+++ myAnalysisTools/development/glue_config/.glue/dls/dls.py
@@ -7,9 +7,11 @@ from matplotlib.patches import Circle, Rectangle, Arc
 from matplotlib.lines import Line2D
 import numpy as np
 from lib.analysis_library import vectorized_binned_statistic_dd
+from scipy.stats import binned_statistic
+import pickle
 
 def t_func(r):
-	temp = array([pickle.loads(i) for i in r])
+	temp = np.array([pickle.loads(i) for i in r])
 	
 	x=temp[:,0]
 	y=temp[:,1]
@@ -21,23 +23,23 @@ def t_func(r):
 
 	myLength=len(y)
 	threshold=0.05
-	ySortedIndex = argsort(y)
+	ySortedIndex = np.argsort(y)
 	y = y[ySortedIndex][int(threshold*myLength):int(myLength*(1-1*threshold))]
 	x = x[ySortedIndex][int(threshold*myLength):int(myLength*(1-1*threshold))]
 	
-	xSortedIndex = argsort(x)
+	xSortedIndex = np.argsort(x)
 	y = y[xSortedIndex][int(threshold*myLength):int(myLength*(1-1*threshold))]
 	x = x[xSortedIndex][int(threshold*myLength):int(myLength*(1-1*threshold))]
 
 	#print(str(len(x))+", "+str(len(y))+", "+str(len(r)))
 	#print(str(len(x))+", "+str(len(y)))
 	
-	myCov = cov(x,y)
+	myCov = np.cov(x,y)
 	simple_slope = myCov[0,1]/myCov[0,0]
 	
-	non_serialized_result = array([mean(y*1.0/x),mean(y)/mean(1.0*x),simple_slope])
+	non_serialized_result = np.array([np.mean(y*1.0/x),np.mean(y)/np.mean(1.0*x),simple_slope])
 	#serialized_result = pickle.dumps(non_serialized_result)
-	dict_result = {'shot_by_shot_median':median(y*1.0/x),'shot_by_shot':mean(y*1.0/x),'median':median(y)/median(1.0*x),'averaged':mean(y)/mean(1.0*x),'simple_slope':simple_slope}
+	dict_result = {'shot_by_shot_median':np.median(y*1.0/x),'shot_by_shot':np.mean(y*1.0/x),'median':np.median(y)/np.median(1.0*x),'averaged':np.mean(y)/np.mean(1.0*x),'simple_slope':simple_slope}
 
 	#IPython.embed()
 	return dict_result
@@ -46,11 +48,11 @@ class dls_viewer(CustomViewer):
 	name = 'dls_viewer'
 	x = 'att(/GMD)'	#this switch swaps the x and y axes.
 	y = 'att(/acqiris2)'	#need to figure out how to make this programable.  Replaced x and y with pop and viv, then need to rename in glue.
-	bins = (10, 100)
+	bins = (25, 300)
 	#more_bins =(-10,10)	#this adds bins 
 	z = 'att(/atm_corrected_timing)'
 	ephoton = 'att(/ebeam/photon_energy)'
-	hitrate = False
+	shot_by_shot = False
 	color = ['Reds', 'Purples']
 	#hit = 'att(shot_made)'
 
@@ -72,13 +74,28 @@ class dls_viewer(CustomViewer):
 		axes.plot(myHistogram[1][:-1],the_dls[::-1],marker='o',linewidth=0)
 
 
-	def plot_subset(self, axes, x, y,z, style,bins):
-		myHistogramW=np.histogram(z,bins=np.arange(326.9,347.1,0.075),weights = np.nan_to_num(y*x*1.0/(x**2+1e-12)))
-		myHistogram=np.histogram(z,bins=np.arange(326.9,347.1,0.075))
+	def plot_subset(self, axes, x, y,z, style,bins,shot_by_shot):
+		binSize = (347.1-326.9)/bins
+		tEdges = np.arange(326.9,347.1,binSize)
+		myHistogramW=np.histogram(z,bins=tEdges,weights = np.nan_to_num(y*x*1.0/(x**2+1e-12)))
+		myHistogram=np.histogram(z,bins=tEdges)
 		the_dls= myHistogramW[0]/myHistogram[0]
 		the_dls -= np.mean(the_dls)
 		the_dls/=np.std(the_dls)
-		axes.plot(myHistogram[1][:-1],the_dls[::-1],mec=style.color,mfc=style.color,marker='o',linewidth=0)
+		#axes.plot(myHistogram[1][:-1],the_dls[::-1],mec=style.color,mfc=style.color,marker='o',linewidth=0)
+
+		myValues = np.array([x,y]).transpose()
+		if(shot_by_shot):
+			myStatistic = 'shot_by_shot'
+		else:
+			myStatistic = 'averaged'
+
+		if(len(z)!=0):
+			myStats = vectorized_binned_statistic_dd(z,myValues,bins=[tEdges],statistic=t_func)	#square brackets around tEdges is important
+			myStats[myStatistic]-=np.mean(myStats[myStatistic])
+			myStats[myStatistic]/=np.std(myStats[myStatistic])
+			axes.plot(tEdges[:-1],myStats[myStatistic][::-1],mec=style.color,mfc=style.color,marker='o',linewidth=0)
+
 
 	def setup(self, axes):
 		temp =0 
