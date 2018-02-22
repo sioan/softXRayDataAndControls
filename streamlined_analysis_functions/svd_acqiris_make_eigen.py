9bc137143a95548e3fea8e80e29b1e4b7d6104a7
Sioan <zohar.sioan@gmail.com>
2018-02-22 14:23:14 -0800

    clunky svd for psana. only single core, not mpi

--- streamlined_analysis_functions/svd_acqiris_make_eigen.py
+++ streamlined_analysis_functions/svd_acqiris_make_eigen.py
@@ -5,6 +5,7 @@ from scipy.optimize import curve_fit
 from scipy.signal import savgol_filter
 
 def make_acq_svd_background(detectorObject,thisEvent,previousProcessing):
+	#waveforms are too large to write even several hundred to file.  doing a recusrive svd
 	selfName = detectorObject['self_name']
 	
 	if (0==len(previousProcessing.keys())):
@@ -14,16 +15,36 @@ def make_acq_svd_background(detectorObject,thisEvent,previousProcessing):
 
 	if(None is detectorObject[selfName](thisEvent)):
 		#fit_results = {'amplitude':popt[2],'uncertainty_cov':pcov[2,2]}
-		return None
+		return eigen_wave_forms
 
 	x = detectorObject[selfName](thisEvent)[1][0]
+
+	#
+
 	for i in arange(len(detectorObject[selfName](thisEvent)[0])):
 
 		y = detectorObject[selfName](thisEvent)[0][i]
+		try:
+			#IPython.embed()
+			temp = vstack([eigen_wave_forms['ch'+str(i)],y])
 
-		eigen_wave_forms['ch'+str(i)] = {}
+		except ValueError:
+			temp = y
+			eigen_wave_forms['ch'+str(i)] = y
+
+		try:
+			
+			#u,s,v = svd(temp)
+			my_eig = eig(dot(temp,temp.transpose()))
+			v = dot(my_eig[1],temp)
+
+			eigen_wave_forms['ch'+str(i)] = v[:11]
+	
+		except LinAlgError:
+			pass
 
 
+	return eigen_wave_forms
 
 	return eigen_wave_forms
 
