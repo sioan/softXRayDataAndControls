c52c13939bf41f0cf4a81d5b9f3f721452377305
Sioan <zohar.sioan@gmail.com>
2018-03-20 11:01:12 -0700

    l.l.l

--- interactiveExtractionValidator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ipython -i /reg/neh/home/sioan/Desktop/git_version_controlled/auto_xtc_hdf5_converter/extractorTester.py -- -f "$@"
-#ipython -i /reg/g/psdm/sw/hutch/sxd/auto_xtc_hdf5_converter/extractorTester.py -- -f "$@"
