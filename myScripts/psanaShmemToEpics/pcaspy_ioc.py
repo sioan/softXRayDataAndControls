844e391f94e130d0703f016967a3d5402137c4cc
Scott Stubbs <sstubbs@slac.stanford.edu>
2018-03-20 17:48:11 -0700

    Adding hutch argument to pcaspy ioc

--- myScripts/psanaShmemToEpics/pcaspy_ioc.py
+++ myScripts/psanaShmemToEpics/pcaspy_ioc.py
@@ -1,10 +1,25 @@
 from pcaspy import SimpleServer, Driver
+import argparse
+import sys
 
 class myDriver(Driver):
     def __init__(self):
         super(myDriver, self).__init__()
 
-prefix = 'SXR:NOTE:'
+parser = argparse.ArgumentParser(description='PCASpy Server holding notebook waveform (array) PV. Run on hutch control or IOC node.')
+parser.add_argument('hutch', metavar='HUTCH', help='Name of hutch (AMO or SXR)')
+try:
+	args=parser.parse_args()
+except:
+	pass #Likely no arguments passed in
+if args.hutch.lower() == "amo":
+	prefix = "AMO:NOTE:"
+elif args.hutch.lower() == "sxr":
+	prefix = "SXR:NOTE:"
+else:
+	print "Error, hutch %s not supported!" % args.hutch
+        sys.exit()
+
 pvdb = {
     'RAND' : {
         'prec' : 3,
