22d312be47ab7efd6af02c2d31cb2773e6c3383b
Sioan Zohar <zohar.sioan@gmail.com>
2018-04-10 08:18:47 -0700

    working gaussian fitting for sxrlr0716

--- experimentSpecificFiles/sxrlr0716/analysis.py
+++ experimentSpecificFiles/sxrlr0716/analysis.py
@@ -26,48 +26,66 @@ width_list = []
 my_hdf5_object.close()
 
 def gaussian(x, x0,sigma,a,offset):
-	sigma_min = 0.5
+	sigma_min = 1
 	#return a*exp(-(x-x0)**2/(2*sigma**2))+4e-2
-	return a*exp(-(x-x0)**2/(2*(sigma**2+sigma_min**2)))+offset
+	return a*exp(-(x-1.0*x0)**2/(2*(sigma**2+sigma_min**2)))+offset
 
 def get_peaks(my_spectra):
 	energy = arange(len(my_spectra))
-	num_peaks = 10
+	num_peaks = 3
 	working_area = 10
-	initial_positions = argsort(my_spectra)[:num_peaks]
-	initial_amplitudes = my_spectra[initial_positions]
+	initial_positions = argsort(my_spectra)[-num_peaks:]
+	initial_amplitudes = my_spectra[initial_positions]*1.0
+	known_good_region = 1200
 
-	my_fits = [0,0,0,0]
+	my_fits = [0,0,0,0,0]
 
 	for i in arange(num_peaks):
 		try:
-			if(initial_positions[i]>working_area and initial_positions[i]<(len(energy)-working_area)):
+			if((initial_positions[i]>working_area and initial_positions[i]<(len(energy)-working_area)) and initial_positions[i]< known_good_region):
 				y = my_spectra[initial_positions[i]-working_area:initial_positions[i]+working_area]
 				x = energy[initial_positions[i]-working_area:initial_positions[i]+working_area]
 	
-				popt,pcov = curve_fit(gaussian,y,x,p0=[initial_positions[i],0.1,initial_amplitudes[i],0.0])
+				popt,pcov = curve_fit(gaussian,y,x,p0=[initial_positions[i]*1.0,0.1,initial_amplitudes[i],0.0])
 		
-				my_fits = vstack([my_fits,[popt[0]+initial_positions[i],popt[1],popt[2],popt[3]]])
+				my_fits = vstack([my_fits,[initial_positions[i],popt[0],popt[1],popt[2],popt[3]]])
+
+				my_spectra[initial_positions[i]-working_area:initial_positions[i]+working_area]-=0
+			else:
+				my_fits = vstack([my_fits,[initial_positions[i],0,0,0,0]])
+
 		except RuntimeError:
 			#IPython.embed()
-			pass
+			#pass
+			my_fits = vstack([my_fits,[initial_positions[i],0,0,0,0]])
 
 	return my_fits[1:]
 
 
 my_amplitudes = array([])
-my_energies = array([])
+my_energies_fine = array([])
+my_energies_coarse = array([])
 my_sigmas = array([])
 
 my_counter = 0
 for my_spectra in my_dict['andor_spec/image']:
+	
+	my_spectra=my_spectra*1.0
+	my_median= median(my_spectra)
+	for i in arange(4):
+		my_spectra[i::4]=my_spectra[i::4]-median(my_spectra[i::4])+my_median	#fourier filtering
+
 	my_fit = get_peaks(my_spectra)
-	my_amplitudes = append(my_amplitudes, my_fit[:,2])	
-	my_energies = append(my_energies,my_fit[:,0])
-	my_sigmas = append(my_sigmas,my_fit[:,1])
+	my_amplitudes = append(my_amplitudes, my_fit[:,3])	
+	my_energies_coarse = append(my_energies_coarse,my_fit[:,0])
+	my_energies_fine = append(my_energies_fine,my_fit[:,1])
+	my_sigmas = append(my_sigmas,my_fit[:,2])
 	if(my_counter%100==0):
 		print(my_counter)
 	my_counter +=1
+
+#hist(my_energies_coarse[my_amplitudes*(my_sigmas**2+1.0**2)**0.5>345],bins=arange(2048))
+#plot(sum(array([(i>345)*1.0 for i in my_dict['andor_spec/image']]),axis=0))
 
 #plot(sum(array([(i>340)*1.0 for i in my_dict['andor_spec/image']]),axis=0))
 #show()
