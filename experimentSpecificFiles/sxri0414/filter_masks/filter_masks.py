c883c005d5d066f4fdd43409fe2576b048b8f5c7
Sioan <zohar.sioan@gmail.com>
2018-09-20 14:56:32 -0700

    updating filter file

--- experimentSpecificFiles/sxri0414/filter_masks/filter_masks.py
+++ experimentSpecificFiles/sxri0414/filter_masks/filter_masks.py
@@ -2,14 +2,14 @@ from pylab import *
 
 def sxri0414run60(myDict):
 	myMask = ones(len(myDict['acqiris2/amplitude'])).astype(bool)
-	myMask = myMask * (array(myDict['TSS_OPAL/pixelTime'])>0)	#excluding bad time tool data
+	#myMask = myMask * (array(myDict['TSS_OPAL/pixelTime'])>0)	#excluding bad time tool data
 	myMask = myMask * (array(myDict['acqiris2/amplitude'])>0.002)	#excluding bad acqiris data
 	myMask = myMask * (array(myDict['acqiris2/amplitude'])<0.90)	#excluding bad acqiris data
 	myMask = myMask * (array(myDict['GMD'])>0.00001)	#excluding bad gmd data
 	myMask = myMask * (array(myDict['GMD'])<0.0012)	#excluding bad gmd data
 	myMask = myMask * (array(myDict['evr/code_162'])==0)
-	myMask = myMask * (array(myDict['TSS_OPAL/positionFWHM'])<50)
-	myMask = myMask * (array(myDict['TSS_OPAL/positionFWHM'])>10)
+	#myMask = myMask * (array(myDict['TSS_OPAL/positionFWHM'])<50)
+	#myMask = myMask * (array(myDict['TSS_OPAL/positionFWHM'])>10)
 	myMask = myMask * (array(myDict['ebeam/photon_energy'])>900)
 	myMask = myMask * (array(myDict['ebeam/photon_energy'])<930)
 	#myMask = myMask * (array(myDict['fiducials']%4==3)	#this has no effect on fourier components
